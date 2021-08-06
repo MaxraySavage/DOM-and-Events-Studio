06b7a65a70648790be40667dcebc6a98fb5aa771
Maxray Savage <MaxraySavage@users.noreply.github.com>
2021-08-05 20:25:20 -0400

    implements take off click handler

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+window.addEventListener('load', function() {
+    const takeOffBtn = document.getElementById('takeoff');
+    takeOffBtn.addEventListener('click', function() {
+        const shuttleConfirmation = window.confirm('Confirm that the shuttle is ready for takeoff.');
+        if (shuttleConfirmation) {
+            const flightStatus = document.getElementById('flightStatus');
+            flightStatus.innerHTML = 'Shuttle in flight.';
+            const shuttleFlightScreen = document.getElementById('shuttleBackground');
+            shuttleFlightScreen.style.backgroundColor = 'blue';
+            const shuttleHeight = document.getElementById("spaceShuttleHeight");
+            shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML) + 10000;
+        }
+    });
+});
